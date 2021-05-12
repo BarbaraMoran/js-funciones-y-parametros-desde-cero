8f61274c57100130caec7fc27ddecbf99e4ff970
BarbaraMoran <barbaramoranrr@gmail.com>
2021-05-12 21:05:03 +0200

    add fase/4

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 "use strict";
 
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+const randomNumber = getRandomNumber(10);
+
 function writeThis(word, number) {
   for (let i = 0; i < number; i++) {
     console.log(word);
   }
 }
 
-writeThis("patata", 10);
-writeThis("aguacate", 7);
-writeThis("Pizza", 50);
+writeThis("patata", randomNumber);
+writeThis("aguacate", randomNumber);
+writeThis("Pizza", randomNumber);
