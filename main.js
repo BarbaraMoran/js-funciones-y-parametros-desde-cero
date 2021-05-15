aca8e2e3cd6ced37db9d61364ffb9308e1beebf4
BarbaraMoran <barbaramoranrr@gmail.com>
2021-05-15 19:53:12 +0200

    fase/4 completed

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 "use strict";
 
+function writeThis(word, getRandomNumber) {
+  for (let i = 0; i < getRandomNumber; i++) {
+    console.log(word);
+  }
+}
+
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-
-writeThis('patata',?);
-writeThis('aguacate'?);
-writeThis('pizza',?);
-
-
+writeThis("patata", getRandomNumber(10));
+writeThis("aguacate", getRandomNumber(10));
+writeThis("pizza", getRandomNumber(10));
