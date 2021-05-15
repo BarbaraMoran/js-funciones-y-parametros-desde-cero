ca16c0dc7e742f8571fbcdb4c755edcd63a6471d
BarbaraMoran <barbaramoranrr@gmail.com>
2021-05-15 19:38:02 +0200

    in progress

--- main.js
+++ main.js
@@ -1,31 +1,12 @@
 "use strict";
 
-const myWordList = [
-  {
-    text: "Pencil",
-    total: 6,
-  },
-  {
-    text: "Thermo",
-    total: 2,
-  },
-  {
-    text: "TV",
-    total: 8,
-  },
-  {
-    text: "Phone",
-    total: 4,
-  },
-];
-
-const text = myWordList.text;
-console.log(text);
-
-function writeMyArray(myWordList) {
-  for (let i = 0; i < myWordList.length; i++) {
-    console.log(text);
-  }
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
 }
 
-writeMyArray(myWordList);
+
+writeThis('patata',?);
+writeThis('aguacate'?);
+writeThis('pizza',?);
+
+
