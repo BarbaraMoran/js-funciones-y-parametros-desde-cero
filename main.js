ffe67e52e9d55b20c0c236132ea5b481fa09f930
BarbaraMoran <barbaramoranrr@gmail.com>
2021-05-17 17:51:15 +0200

    erase commented code

--- main.js
+++ main.js
@@ -21,39 +21,3 @@ function writeMyArray() {
 }
 
 writeMyArray();
-
-/*
-const myWordList = [
-  {
-    text: "Pencil",
-    total: 6,
-  },
-  {
-    text: "Thermo",
-    total: 2,
-  },
-  {
-    text: "TV",
-    total: 8,
-  },
-  {
-    text: "Phone",
-    total: 4,
-  },
-];
-
-function writeMyArray(arr) {
-  let text;
-  let total;
-  for (let i = 0; i < arr.length; i++) {
-    text = arr[i].text;
-    total = arr[i].total;
-
-    for (let i = 0; i < total; i++) {
-      console.log(text);
-    }
-  }
-}
-
-writeMyArray(myWordList);
-*/
