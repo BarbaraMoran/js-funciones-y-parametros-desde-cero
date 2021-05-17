8683d27b5bad73657304a3847017db93c05809ae
BarbaraMoran <barbaramoranrr@gmail.com>
2021-05-17 17:43:23 +0200

    add fase/6

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 "use strict";
 
+const url =
+  "https://beta.adalab.es/ejercicios-extra/js-funciones-y-parametros-desde-cero/data.json";
+
+function writeMyArray() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      let text;
+      let total;
+      for (let i = 0; i < data.results.length; i++) {
+        text = data.results[i].text;
+        total = data.results[i].total;
+
+        for (let i = 0; i < total; i++) {
+          console.log(text);
+        }
+      }
+    });
+}
+
+writeMyArray();
+
+/*
 const myWordList = [
   {
     text: "Pencil",
@@ -33,3 +56,4 @@ function writeMyArray(arr) {
 }
 
 writeMyArray(myWordList);
+*/
